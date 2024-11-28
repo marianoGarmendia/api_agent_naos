a015415886270dda9495387a14e188e6941c4e45
Mariano Garmendia <marianogarmendia77@gmail.com>
2024-11-28 14:15:06 -0300

    import relative

--- naos/main.py
+++ naos/main.py
@@ -1,7 +1,7 @@
 
 import os
 import sys
-sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+# sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 
 from langchain_openai import ChatOpenAI
 from langchain_community.tools import TavilySearchResults
